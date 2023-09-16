bf3ca5af43b514a63dc1ddcadd35ec9d2ace8683
Nikita Savin <klaster26@mail.ru>
2023-09-16 15:14:23 +0300

    feat: solve 1 module 4 task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,5 @@
 function checkSpam(str) {
-  // ваш код...
+  if (str.toLowerCase().includes('1xBet'.toLowerCase()) || str.toLowerCase().includes('XXX'.toLowerCase())) {
+      return true;
+  } return false;
 }
