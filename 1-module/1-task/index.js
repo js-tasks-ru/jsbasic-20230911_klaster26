3ae6ca3eff8b1ac0abdcf1a3aa086c358dcc3455
Nikita Savin <klaster26@mail.ru>
2023-09-16 23:04:31 +0300

    fix: solve 1 module 1 task (using loop)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,5 +1,15 @@
-function factorial(n) {
-  if (n === 0) {
-      return 1;
-  } return n * factorial (n - 1);
+function factorial (number) {
+  let i = 1;
+  let result = 1;
+  while (i <= number) {
+      result *= i;
+      i++;
+  } return result;
+}
+
+const factorialWithFor = (number) => { // Стрелочная функция и цикл for для разнообразия;
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+      result *= i;
+  } return result;
 }
