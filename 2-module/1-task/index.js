4fddf5300121d08cdd8bd8ca9598f796b46e9365
Nikita Savin <klaster26@mail.ru>
2023-09-21 14:17:17 +0300

    feat: solve 2 module 1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let key in salaries) {
+      if (Boolean (salaries[key]) && typeof salaries[key] == 'number' && Math.abs (salaries[key]) != Infinity) {
+      result += salaries[key];
+      }
+  } return result;
 }
