4ddaf0f87f68115c16c9c7eddef31914c1b7caec
Nikita Savin <klaster26@mail.ru>
2023-09-23 20:27:02 +0300

    feat: solve 3 module 3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  str = str.split('-');
+  let result = str.map ((word, index) => {
+      if (index == 0) {
+          return `${word}`;
+      } return `${word[0].toUpperCase()}${word.slice(1)}`;
+  }).join('');
+  return result;
 }
