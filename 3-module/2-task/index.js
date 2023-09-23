7befe388e053f59575647cdd9fe81e66f0cb7029
Nikita Savin <klaster26@mail.ru>
2023-09-23 19:14:42 +0300

    feat: solve 3 module 2 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,18 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  let result = [];
+  arr.map ( num => {
+      if (num >= a && num <= b) {
+          result.push (num);
+      }
+  });
+  return result;
+}
+
+function filterRange(arr, a, b) { // Либо аналогичное решение с использованием метода reduce().
+                                  // Это я для себя прорабатываю. Ненавижу методы массивов и пытаюсь это побороть :D
+  return arr.reduce ( (result, num) => {
+      if (num >= a && num <= b) {
+          result.push(num);
+      } return result;
+  }, []);
 }
