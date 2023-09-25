ad1c11ef2c3cc33aba4bb702b96f338145ebfc44
Nikita Savin <klaster26@mail.ru>
2023-09-25 18:03:23 +0300

    feat: solve 3 module 4 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,17 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = [];
+  users.map ((user) => {
+    if (user.age <= age) {
+      result.push(`${user.name}, ${user.balance}`);
+    }
+  }); return (result.join('\n'));
 }
+
+function showSalary(users, age) { // Per methodos ad astra!
+  let array = users.reduce ((result, user) => {
+  if (user.age <= age) {
+    result.push(`${user.name}, ${user.balance}`);
+  }
+  return result;
+}, []); return (array.join('\n'));
+};
