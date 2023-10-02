409cefa57a5539252cd7cf7c2f7caea4ca3101cb
Nikita Savin <klaster26@mail.ru>
2023-10-02 21:08:52 +0300

    feat: solve 4 module 3 task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+     if (!table.rows[i].cells[3].hasAttribute('data-available')) {
+       table.rows[i].setAttribute ('hidden', 'hidden');
+     }
+
+     else if (table.rows[i].cells[3].getAttribute('data-available') == 'true') {
+        table.rows[i].classList.add ('available');
+     } else {
+       table.rows[i].classList.add ('unavailable');
+     }
+
+    if (table.rows[i].cells[2].innerHTML == 'm') {
+      table.rows[i].classList.add (`male`);
+    } else {
+      table.rows[i].classList.add (`female`);
+    }
+
+    if (Number(table.rows[i].cells[1].innerHTML) < 18) {
+      table.rows[i].style.textDecoration = 'line-through';
+    }
+  }
 }
