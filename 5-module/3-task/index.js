b65f1772bb28d7c5c31938fca21eeb33777d5165
Nikita Savin <klaster26@mail.ru>
2023-10-07 19:48:20 +0300

    feat: solve 5 module 3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  let initPosition = 0;
+  const rightStringDiv = document.querySelector('.carousel__arrow_right');
+  const leftStringDiv = document.querySelector('.carousel__arrow_left');
+  const carouselInner = document.querySelector('.carousel__inner');
+
+  if (initPosition >= 0) {
+    leftStringDiv.style.display = 'none';
+    };
+
+    document.querySelector ('.carousel').addEventListener ('click', (event) => {
+    if (event.target.getAttribute ('class') == 'carousel__arrow carousel__arrow_right'
+    || event.target.parentElement.getAttribute ('class') == 'carousel__arrow carousel__arrow_right') {
+      carouselInner
+      .style.transform = `translateX(${initPosition - carouselInner.offsetWidth}px)`;
+      initPosition -= carouselInner.offsetWidth;
+      }
+
+
+    if (event.target.getAttribute ('class') == 'carousel__arrow carousel__arrow_left'
+    || event.target.parentElement.getAttribute ('class') == 'carousel__arrow carousel__arrow_left') {
+      carouselInner
+      .style.transform = `translateX(${initPosition + carouselInner.offsetWidth}px)`;
+      initPosition += carouselInner.offsetWidth;
+      };
+
+      if (initPosition <= - carouselInner.offsetWidth * 3) {
+        rightStringDiv.style.display = 'none';
+      }
+      else {
+        rightStringDiv.style.display = '';
+        }
+
+      if (initPosition >= 0) {
+        leftStringDiv.style.display = 'none';
+        }
+      else {
+        leftStringDiv.style.display = '';
+      }
+  });
 }
