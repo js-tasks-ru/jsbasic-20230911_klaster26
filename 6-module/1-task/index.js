c6b480e1b972c1e618078b861561b0a9ced8991a
Nikita Savin <klaster26@mail.ru>
2023-10-08 17:35:57 +0300

    feat: solve 6 module 1 task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,49 @@
  */
 export default class UserTable {
   constructor(rows) {
+    if (!document.querySelector ('.table-head')) {
+      let table = document.createElement ('table');
+      let tableHead = document.createElement ('thead');
+      table.classList.add ('users-table');
+      tableHead.classList.add ('table-head');
+      document.body.append (table);
+      table.append (tableHead);
+      tableHead.insertAdjacentHTML ('beforeend',
+      `<thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+        </thead>`);
+      }
+    const usersTable = document.querySelector ('.users-table');
+    const buttonTemplate = `<button class='closing-button'>X</button>`;
+    usersTable.append (document.createElement ('tbody'));
+    usersTable.querySelector('tbody').classList.add ('users');
+    const tableBody = document.querySelector ('.users');
+
+    for (let user of rows) {
+      let tableRow = document.createElement ('tr');
+      tableBody.append (tableRow);
+      for (let parameter in user) {
+        let rowCell = document.createElement ('td');
+        tableRow.append (rowCell);
+        rowCell.innerHTML = `${user[parameter]}`;
+      }
+      let rowCell = document.createElement ('td');
+      tableRow.append (rowCell);
+      rowCell.insertAdjacentHTML ('beforeend', buttonTemplate);
+    }
+    tableBody.addEventListener ('click', (event) => {
+      if (event.target.getAttribute ('class') == 'closing-button') {
+        event.target.closest ('tr').remove();
+      }
+    })
+  };
+  get elem() {
+    return document.querySelector ('.users-table');
   }
 }
