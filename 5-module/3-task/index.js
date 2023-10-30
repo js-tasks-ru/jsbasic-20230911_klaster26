21566668238881da668f8311852fadfb3e630189
Nikita Savin <klaster26@mail.ru>
2023-10-30 19:58:43 +0300

    refactor: minor refactor code in 5 module 3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -6,36 +6,33 @@ function initCarousel() {
 
   if (initPosition >= 0) {
     leftStringDiv.style.display = 'none';
-    };
+  }
 
-    document.querySelector ('.carousel').addEventListener ('click', (event) => {
-    if (event.target.getAttribute ('class') == 'carousel__arrow carousel__arrow_right'
-    || event.target.parentElement.getAttribute ('class') == 'carousel__arrow carousel__arrow_right') {
+  document.querySelector('.carousel').addEventListener('click', (event) => {
+    if (event.target.getAttribute('class') == 'carousel__arrow carousel__arrow_right'
+    || event.target.parentElement.getAttribute('class') == 'carousel__arrow carousel__arrow_right') {
       carouselInner
       .style.transform = `translateX(${initPosition - carouselInner.offsetWidth}px)`;
       initPosition -= carouselInner.offsetWidth;
-      }
+    }
 
-
-    if (event.target.getAttribute ('class') == 'carousel__arrow carousel__arrow_left'
-    || event.target.parentElement.getAttribute ('class') == 'carousel__arrow carousel__arrow_left') {
+    if (event.target.getAttribute('class') == 'carousel__arrow carousel__arrow_left'
+    || event.target.parentElement.getAttribute('class') == 'carousel__arrow carousel__arrow_left') {
       carouselInner
       .style.transform = `translateX(${initPosition + carouselInner.offsetWidth}px)`;
       initPosition += carouselInner.offsetWidth;
-      };
+    }
 
-      if (initPosition <= - carouselInner.offsetWidth * 3) {
-        rightStringDiv.style.display = 'none';
-      }
-      else {
-        rightStringDiv.style.display = '';
-        }
+    if (initPosition <= -carouselInner.offsetWidth * 3) {
+      rightStringDiv.style.display = 'none';
+    } else {
+      rightStringDiv.style.display = '';
+    }
 
-      if (initPosition >= 0) {
-        leftStringDiv.style.display = 'none';
-        }
-      else {
-        leftStringDiv.style.display = '';
-      }
+    if (initPosition >= 0) {
+      leftStringDiv.style.display = 'none';
+    } else {
+      leftStringDiv.style.display = '';
+    }
   });
 }
